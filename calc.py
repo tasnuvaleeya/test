1583585dcd2b117cf5bfd64fecd77f7d91ce0bfb
tasnuvaleeya <tasnuva2606@gmail.com>
2018-11-22 13:14:16 +0600

    modification to divide function

--- calc.py
+++ calc.py
@@ -15,7 +15,7 @@ def multiply(x, y):
 
 def divide(x, y):
     """Divide Function"""
-    if y == 0:
-        raise ValueError('Can not divide by zero!')
+    # if y == 0:
+    #     raise ValueError('Can not divide by zero!')
     return x / y
 q
